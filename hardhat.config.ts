0fc906ef8daa93825044a91a3484aa7987d713ca
incmsx <kondratovich2431@gmail.com>
2025-07-26 23:36:22 +0300

    deploy and verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import 'solidity-coverage'
+import 'solidity-coverage';
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY as string],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY as string,
+    },
+  }
 };
 
 export default config;
